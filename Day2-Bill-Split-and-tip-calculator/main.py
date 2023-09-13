eb0a27db9ba512139423f683d3d2104236ba7463
AmaliEffendy <amali.effendy@gmail.com>
2023-09-13 19:08:09 +0800

    Second Project

---
+++ Day2-Bill-Split-and-tip-calculator/main.py
@@ -0,0 +1,25 @@
+#100DaysofPython - Day 2 Project  - Bill Split and Tip Calculator
+
+#Introduction
+print("Welcome to the Bill Split and Tip Calculator")
+
+#Total Cost
+total = input("Please input the total bill cost: ")
+
+#Split Cost
+split = input("Please input how many people will split the bill: ")
+
+#Tip Percentage
+tip = input("Please input the tip percentage you would like to give: (Input the number without the percentage) ")
+
+#refactoring
+float_total = float(total)
+float_split = float(split)
+float_tip = float(tip)
+
+#result
+total_result = float(float_tip / 100 * float_total + float_total)
+split_result = float(total_result / float_split)
+rounded_result = round(split_result, 2)
+
+print(f"Cost that should be split is: $ {rounded_result}")
